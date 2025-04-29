46d6c4ec19b420ad4a739cb44423da57cba2f2f9
axpelman <axpelman@gmail.com>
2025-04-29 12:09:27 +0300

    добавил коментарий

--- Script/TG__massage_collector.py
+++ Script/TG__massage_collector.py
@@ -15,6 +15,7 @@ from telethon.tl.types import PeerChannel
 # Europe/Moscow (Москва), Europe/Kyiv (Киев), Asia/Tbilisi (Тбилиси)
 # Asia/Dubai (Дубай), Asia/Tokyo (Токио), Europe/London (Лондон)
 # America/New_York (Нью-Йорк), Asia/Shanghai (Шанхай)
+# что-то еще 
 
 # Default timezone (can be changed):
 # Examples of popular timezones:
